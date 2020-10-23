9a16c9b536ad8a40eaf2c617cb53730f97b1e06c
moju zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2020-10-23 11:14:57 +0900

    Add youtube_bb curation script

---
+++ datasets/yt_bb/curate.py
@@ -0,0 +1,177 @@
+import os
+from os.path import join, isdir
+from concurrent import futures
+import json
+import pickle
+import sys
+import cv2
+import pandas as pd
+import numpy as np
+from download import parse_annotations
+
+sys.path.append('../')
+import utils
+from yt_utils import parse_annotations, d_sets
+
+# The data sets to be downloaded
+YY_TB_base_path = "./youtube_bb"
+save_base_path = join(YY_TB_base_path, 'Curation')
+
+debug = False
+
+# Download and cut a clip to size
+def crop_video(vid):
+
+    d_set_dir = vid.clips[0].d_set_dir
+
+    for clip in vid.clips:
+        name = clip.name
+        yt_id = clip.yt_id
+        class_id = clip.class_id
+        obj_id = clip.obj_id
+        d_set_dir = clip.d_set_dir
+
+        for index in range(len(clip.timestamps)):
+            presence = clip.presences[index]
+            timestamp = clip.timestamps[index]
+            bbox = clip.bboxes[index]
+
+            if presence == 'absent':
+                continue
+
+            class_dir = join(d_set_dir, str(clip.class_id))
+            frame_path = class_dir+ '/'  + yt_id +  '_' + str(timestamp) + \
+                         '_' + str(class_id) + '_' + str(obj_id) + '.jpg'
+            # Verify that the video has been downloaded. Skip otherwise
+            #print(frame_path)
+            if not os.path.exists(frame_path):
+                continue
+
+            image = cv2.imread(frame_path)
+
+            avg_chans = np.mean(image, axis=(0, 1))
+
+            # Uncomment lines below to print bounding boxes on downloaded images
+            h, w = image.shape[:2]
+            x1 = bbox[0] * w
+            x2 = bbox[1] * w
+            y1 = bbox[2] * h
+            y2 = bbox[3] * h
+
+            if x1 < 0 or x2 < 0 or y1 < 0 or y2 < 0 or y2 < y1 or x2 < x1:
+                continue
+
+
+            # Make the class directory if it doesn't exist yet
+            # video_crop_base_path = join(save_base_path, d_set_dir.split("/")[-1], str(class_id), yt_id) # with dataset type
+            video_crop_base_path = join(save_base_path, str(class_id), yt_id)
+
+            if not isdir(video_crop_base_path): os.makedirs(video_crop_base_path)
+
+            # Save the extracted image
+            bbox = np.round(np.array([x1, y1, x2, y2])).astype(np.uint16)
+
+            z, x = utils.crop_image(image, bbox, padding=avg_chans)
+            #cv2.imwrite(join(crop_class_dir, '{:06d}.{:02d}.z.jpg'.format(int(timestamp)/1000, int(obj_id))), z)
+            cv2.imwrite(join(video_crop_base_path, '{:06d}.{:02d}.x.jpg'.format(int(timestamp/1000), int(obj_id))), x)
+
+
+# Parse the annotation csv file and schedule downloads and cuts
+def image_curation(num_threads=24):
+    """Crop the entire youtube-bb data set into `crop_path`.
+    """
+    # For each of the two datasets
+    for d_set in d_sets:
+
+        js = {}
+
+        # Parse csv data using pandas
+        annotations,clips,vids = parse_annotations(d_set, YY_TB_base_path)
+
+        # Crop in parallel threads giving
+        print("Cropping images on {} videos for d_set".format(len(vids)))
+
+        if debug: # only test with one video:
+            vids = vids[:10]
+
+        # debug
+        #for vid in vids:
+        #    crop_video(vid)
+        #raise
+
+        with futures.ProcessPoolExecutor(max_workers=num_threads) as executor:
+            fs = [executor.submit(crop_video, vid) for vid in vids]
+            for i, f in enumerate(futures.as_completed(fs)):
+                sys.stderr.write("Curate video: {} / {} \r".format(i, len(vids)))
+
+        for i, vid in enumerate(vids):
+
+            sys.stderr.write("dumping video {} / {} in {} \r".format(i, len(vids), d_set))
+            image_w = 0
+            image_h = 0
+
+            for clip in vid.clips:
+
+                name = clip.name
+                yt_id = clip.yt_id
+                class_id = clip.class_id
+                obj_id = clip.obj_id
+                d_set_dir = clip.d_set_dir
+
+                for index in range(len(clip.timestamps)):
+
+                    presence = clip.presences[index]
+                    timestamp = clip.timestamps[index]
+                    x1, x2, y1, y2 = clip.bboxes[index]
+
+                    if presence == 'absent':
+                        continue
+
+                    if x1 < 0 or x2 < 0 or y1 < 0 or y2 < 0 or y2 < y1 or x2 < x1:
+                        continue
+
+                    frame = '%06d' % (int(timestamp) / 1000)
+                    obj = '%02d' % (int(obj_id))
+                    # video = join(d_set, str(class_id) , yt_id) # with dataset type
+                    video = join(str(class_id) , yt_id)
+                    video_crop_base_path = join(save_base_path, video)
+
+                    video_file = join(video_crop_base_path, '{}.{}.x.jpg'.format(frame, obj))
+                    # print(video_file)
+                    if not os.path.exists(video_file):
+                        continue
+
+                    if image_w == 0 or image_h == 0:
+                        # TODO: any faster way to know the image shape
+                        class_dir = join(d_set_dir, str(clip.class_id))
+                        frame_path = class_dir+ '/'  + yt_id +  '_' + str(timestamp) + \
+                                     '_' + str(class_id) + '_' + str(obj_id) + '.jpg'
+                        assert os.path.exists(frame_path)
+                        image = cv2.imread(frame_path)
+                        image_h, image_w = image.shape[:2]
+
+                    x1 = x1 * image_w
+                    x2 = x2 * image_w
+                    y1 = y1 * image_h
+                    y2 = y2 * image_h
+
+                    if video not in js:
+                        js[video] = {}
+                    if obj not in js[video]:
+                        js[video][obj] = {}
+                    js[video][obj][frame] = [x1, y1, x2, y2]
+
+        if 'yt_bb_detection_train' == d_set:
+            json.dump(js, open(join(save_base_path, 'train.json'), 'w'), indent=4, sort_keys=True)
+            pickle.dump(js, open(join(save_base_path, 'train.pickle'), 'wb'))
+        else:
+            json.dump(js, open(join(save_base_path, 'val.json'), 'w'), indent=4, sort_keys=True)
+            pickle.dump(js, open(join(save_base_path, 'val.pickle'), 'wb'))
+
+if __name__ == '__main__':
+
+    if len(sys.argv) == 2:
+        image_curation(int(sys.argv[1]))
+    else:
+        image_curation()
+
--- datasets/yt_bb/download.py
+++ datasets/yt_bb/download.py
@@ -1,80 +1,21 @@
 ########################################################################
-# YouTube BoundingBox
-########################################################################
-#
-# This file contains useful functions for downloading, decoding, and
-# converting the YouTube BoundingBox dataset.
-#
+# Download and decode YouTube BoundingBox Video
 ########################################################################
 
 from __future__ import unicode_literals
 from subprocess import check_call
 from concurrent import futures
-from random import shuffle
 from datetime import datetime
-import time
 import subprocess
 import youtube_dl
-import socket
 import os
-import io
 import sys
-import csv
 import cv2
 import numpy as np
+from yt_utils import parse_annotations, find_nearest_index, d_sets
 
-
-# Debug flag. Set this to true if you would like to see ffmpeg errors
+# Debug flag. Set this to true if you only want to download single video and docode it
 debug = False
-
-# The data sets to be downloaded
-d_sets = [
-  'yt_bb_detection_train',
-  'yt_bb_detection_validation'
-]
-
-# Host location of segment lists
-web_host = 'https://research.google.com/youtube-bb/'
-
-# Video clip class
-class video_clip(object):
-  def __init__(self,
-               name,
-               yt_id,
-               start,
-               class_id,
-               obj_id,
-               d_set_dir):
-    # name = yt_id+class_id+object_id
-    self.name     = name
-    self.yt_id    = yt_id
-    self.frames = [start]
-    self.class_id = class_id
-    self.obj_id   = obj_id
-    self.d_set_dir = d_set_dir
-  def print_all(self):
-    print('['+self.name+', '+ \
-              self.yt_id+', '+ \
-              self.frames+', '+ \
-              self.class_id+', '+ \
-              self.obj_id+']\n')
-
-# Video class
-class video(object):
-  def __init__(self,yt_id,first_clip):
-    self.yt_id = yt_id
-    self.clips = [first_clip]
-  def print_all(self):
-    print(self.yt_id)
-    for clip in self.clips:
-      clip.print_all()
-
-
-# Help function to get the index of the element in an array the nearest to a value
-def find_nearest_index(array,value):
-    idx = (np.abs(array-value)).argmin()
-    return idx
-
 
 # Download and cut a clip to size
 def dl_and_cut(vid):
@@ -107,7 +48,7 @@ def dl_and_cut(vid):
 
     for clip in vid.clips:
 
-      labeled_timestamps = np.array(clip.frames) / 1000
+      labeled_timestamps = np.array(clip.timestamps) / 1000
 
       indexes = []
       for label in labeled_timestamps:
@@ -126,7 +67,7 @@ def dl_and_cut(vid):
           ret, image = capture.read()
 
           # Save the extracted image
-          frame_path = class_dir+'/'+ clip.yt_id +'_'+str(clip.frames[i])+\
+          frame_path = class_dir+'/'+ clip.yt_id +'_'+str(clip.timestamps[i])+\
               '_'+str(clip.class_id)+'_'+str(clip.obj_id)+'.jpg'
           cv2.imwrite(frame_path, image)
           #print(frame_path)
@@ -136,93 +77,6 @@ def dl_and_cut(vid):
   if not debug:
     os.remove(d_set_dir+'/'+vid.yt_id+'_temp.mp4')
 
-# Parse the annotation csv file and schedule downloads and cuts
-def parse_annotations(d_set,dl_dir):
-
-  d_set_dir = dl_dir+'/'+d_set+'/'
-
-  # Download & extract the annotation list
-  if not os.path.exists(d_set+'.csv'):
-    print (d_set+': Downloading annotations...')
-    check_call(' '.join(['wget', web_host+d_set+'.csv.gz']),shell=True)
-    print (d_set+': Unzipping annotations...')
-    check_call(' '.join(['gzip', '-d', '-f', d_set+'.csv.gz']), shell=True)
-
-  print (d_set+': Parsing annotations into clip data...')
-
-  # Parse csv data.
-  annotations = []
-  with open((d_set+'.csv'), 'rt') as f:
-    reader = csv.reader(f)
-    annotations = list(reader)
-
-  # Sort to de-interleave the annotations for easier parsing. We use
-  # `int(l[1])` to sort by the timestamps numerically; the other fields are
-  # sorted lexicographically as strings.
-  print(d_set + ': Sorting annotations...')
-
-  # Sort by youtube_id, class, obj_id and then timestamp
-  annotations.sort(key=lambda l: (l[0], l[2], l[4], int(l[1])))
-
-  current_clip_name = ['blank']
-  clips             = []
-
-  # Parse annotations into list of clips with names, youtube ids, start
-  # times and stop times
-  for idx, annotation in enumerate(annotations):
-    yt_id    = annotation[0]
-    class_id = annotation[2]
-    obj_id   = annotation[4]
-
-    clip_name = yt_id+'+'+class_id+'+'+obj_id
-
-    # If this is a new clip
-    if clip_name != current_clip_name:
-
-      #if idx != 0:
-      #  print(clips[-1].name, clips[-1].frames)
-
-      # Add the starting clip
-      clips.append( video_clip(
-        clip_name,
-        yt_id,
-        int(annotation[1]),
-        class_id,
-        obj_id,
-        d_set_dir) )
-
-      # Update the current clip name
-      current_clip_name = clip_name
-
-    else:
-      clips[-1].frames.append(int(annotation[1]))
-
-  # Sort the clips by youtube id
-  clips.sort(key=lambda x: x.yt_id)
-
-  # Create list of videos to download (possibility of multiple clips
-  # from one video)
-  current_vid_id = ['blank']
-  vids = []
-  for clip in clips:
-
-    vid_id = clip.yt_id
-
-    # If this is a new video
-    if vid_id != current_vid_id:
-      # Add the new video with its first clip
-      vids.append( video ( \
-        clip.yt_id, \
-        clip ) )
-    # If this is a new clip for the same video
-    else:
-      # Add the new clip to the video
-      vids[-1].clips.append(clip)
-
-    # Update the current video name
-    current_vid_id = vid_id
-
-  return annotations,clips,vids
 
 # Parse the annotation csv file and schedule downloads and cuts
 def parse_and_sched(dl_dir='videos',num_threads=4):
