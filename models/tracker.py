79fdaac0203dea85ba55ddcc962261174471e3c7
Tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2021-01-29 23:09:36 +0900

    Correct the type for cv2

--- models/tracker.py
+++ models/tracker.py
@@ -266,7 +266,7 @@ class Tracker(object):
 
             # debug for search image:
             start_time = time.time()
-            debug_bbox = box_cxcywh_to_xyxy(torch.round(torch.cat([bbox_ct, bbox_wh])).int())
+            debug_bbox = torch.round(box_cxcywh_to_xyxy(torch.cat([bbox_ct, bbox_wh]))).int()
             rec_search_image = cv2.rectangle(search_image,
                                              (debug_bbox[0], debug_bbox[1]),
                                              (debug_bbox[2], debug_bbox[3]),(0,255,0),3)
